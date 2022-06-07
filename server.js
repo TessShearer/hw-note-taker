20b241a70ac3262e0f67eb19170b6663b9f9778b
TessShearer <shearer.tess@gmail.com>
2022-06-07 12:34:50 -0600

    added env to port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 const fs = require('fs');
